7c24ce0a5d5f3df3f1e14c9b787baa7b36cc90ed
Raunak Singh <admin@Raunaks-MacBook-Air.local>
2020-05-09 20:57:43 -0400

    added record_sheet.csv

--- COVIDdetectorS.py
+++ COVIDdetectorS.py
@@ -7,7 +7,7 @@ import datetime
 import pandas as pd
 serial_port = '/dev/cu.usbmodem141201'
 baud_rate = 9600; #In arduino, Serial.begin(baud_rate)
-write_to_file_path = "output.csv";
+write_to_file_path = "record_sheet.csv";
 
 # output_file = open(write_to_file_path, "w+")
 ser = serial.Serial(serial_port, baud_rate)
