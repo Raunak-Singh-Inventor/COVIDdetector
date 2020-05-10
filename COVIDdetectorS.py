84508447fd38ff78b9a30d8979d1b634a9f406f1
Raunak Singh <Raunak.singh.inventor@gmail.com>
2020-05-09 21:13:42 -0400

    add comments to python recording file

--- COVIDdetectorS.py
+++ COVIDdetectorS.py
@@ -1,34 +1,43 @@
-##############
-## Script listens to serial port and writes contents into a file
-##############
+########################
+# Python script to receive and record the data
+# by Raunak Singh
+########################
+
 ## requires pySerial to be installed 
 import serial
 import datetime
 import pandas as pd
+
+# change the serial port to the one of your arduino
 serial_port = '/dev/cu.usbmodem141201'
-baud_rate = 9600; #In arduino, Serial.begin(baud_rate)
+baud_rate = 9600; # set as serial monitor baud rate
+# data will be written to this csv file
 write_to_file_path = "record_sheet.csv";
 
-# output_file = open(write_to_file_path, "w+")
+# define serial with port and baud rate
 ser = serial.Serial(serial_port, baud_rate)
+# define a new dataframe with one column
 df =pd.DataFrame(["default"],columns=["name"])
-# df["name"] = "HELLO"
-# print(df)
-i=0
-while True:
-    line = ser.readline()
-    line = line.decode("utf-8") #ser.readline returns a binary, convert to string
-    pos =repr(line).find(",")
-    line = repr(line).replace("'","").replace(r"\r","").replace(r"\n","")
-    print(line)
-    name = line[:pos-1]
-    temp =line[pos:]
-    print(name)
-    df.loc[i] = name
-    df["temp"] =temp
-    # output_file.write(line)
-    i+=1
+
+i=0 # counter
+# while input is coming
+while True: 
+    line = ser.readline() # read the line from the serial port
+    line = line.decode("utf-8") #s er.readline returns a binary, convert to string
+    pos =repr(line).find(",") # find comma in the line and mark it's position
+    line = repr(line).replace("'","").replace(r"\r","").replace(r"\n","") # clean the string of unwanted characters
+    print(line) # debugging code
+    name = line[:pos-1] # select the name of user
+    temp =line[pos:] # select temperarure value of user
+    print(name) # debugging code
+    df.loc[i] = name # insert name in name column
+    df["temp"] =temp # insert temp in new temp column
+    i+=1 # increase counter by 1
     
+    # show  th ecode after every 10 lines
     if (i%10==0):
+        # record the time when input recieved
         df["DateTime"] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # write all of this data to record_sheet.csv
         df.to_csv(write_to_file_path)
+#____DONE_____#
